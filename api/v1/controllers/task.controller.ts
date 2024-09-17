7a25bd2db03a93489527e4f6cd24e1f18414d602
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-17 22:40:12 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+  // Find
   interface Find {
     deleted: boolean;
     status?: string;
@@ -15,8 +16,19 @@ export const index = async (req: Request, res: Response) => {
   if (req.query.status) {
     find.status = req.query.status.toString();
   }
+  // End Find
 
-  const tasks = await Task.find(find);
+  // Sort
+  const sort: { [key: string]: any} ={};
+
+  if(req.query.sortKey && req.query.sortValue){
+    const sortKey = req.query.sortKey.toString()
+    sort[sortKey] = req.query.sortValue
+  }
+
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 };
