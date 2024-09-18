2c862842b5458534df30eff6e87666b17667719a
tuan-anh14 <thieutuananh14092k4@gmail.com>
2024-09-18 14:13:34 +0700

    Cài CORS (demo)

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+// import cors from "cors";
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
 
@@ -9,6 +10,15 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+// var corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+// app.use(cors);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
